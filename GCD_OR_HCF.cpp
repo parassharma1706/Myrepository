15151c1aeb3cfaa97cf6be435590bf3d346e13e6
parassharma1706 <122095727+parassharma1706@users.noreply.github.com>
2023-01-28 10:46:24 +0530

    Add files via upload

---
+++ GCD_OR_HCF.cpp
@@ -0,0 +1,20 @@
+//**********************Finding GCD or HCF*******************************
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int a,b;
+	cin>>a>>b;
+// By Euclidean Algorithm
+	while(a>0 && b>0){
+		if(a>b)
+		 a=a%b;
+		else
+		 b=b%a;
+	}
+	if(a==0) 
+	cout<<b;
+	else
+	 cout<<a;
+	
+	return 0;
+}
